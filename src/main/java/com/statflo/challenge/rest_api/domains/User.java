e919db59f4f6fa89367e4fc8586eb7e1c20e4286
Samuel Selvarajah <samueljselvarajah@gmail.com>
2020-01-16 21:40:19 -0500

    - Add user domain - Added hibernate dependency

---
+++ src/main/java/com/statflo/challenge/rest_api/domains/User.java
@@ -0,0 +1,56 @@
+package com.statflo.challenge.rest_api.domains;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "role")
+    private String role;
+
+    public User(){
+    }
+
+    public User(int id, String name, String role){
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", role=" + role + "]";
+    }
+}
